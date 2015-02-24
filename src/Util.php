18bdd3ba3dfb1d9e18ee69f5a5d7229ba7e33aaa
Reüel van der Steege <reuel@pronamic.nl>
2015-02-24 16:33:13 +0100

    Fix for empty return in filter_input( INPUT_SERVER, ... ), see https://bugs.php.net/49184

--- src/Util.php
+++ src/Util.php
@@ -12,10 +12,14 @@ class Pronamic_WP_Pay_Gateways_PayNL_Util {
 	public static function get_ip_address() {
 		if ( filter_has_var( INPUT_SERVER, 'HTTP_X_FORWARDED_FOR' ) ) {
 			return filter_input( INPUT_SERVER, 'HTTP_X_FORWARDED_FOR', FILTER_VALIDATE_IP );
+		} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+			return filter_var( $_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP );
 		}
 
 		if ( filter_has_var( INPUT_SERVER, 'REMOTE_ADDR' ) ) {
 			return filter_input( INPUT_SERVER, 'REMOTE_ADDR', FILTER_VALIDATE_IP );
+		} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) )  {
+			return filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP );
 		}
 
 		return '127.0.0.1';
