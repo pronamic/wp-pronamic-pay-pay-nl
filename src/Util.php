3da5c4566783768d06989ca4fd5d3802985e51c5
Remco Tolsma <info@remcotolsma.nl>
2015-02-27 16:02:39 +0100

    Getting ready for version 1.1.0.

--- src/Util.php
+++ src/Util.php
@@ -6,20 +6,17 @@
  * Copyright: Copyright (c) 2005 - 2014
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.1.0
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_PayNL_Util {
 	public static function get_ip_address() {
-		if ( filter_has_var( INPUT_SERVER, 'HTTP_X_FORWARDED_FOR' ) ) {
-			return filter_input( INPUT_SERVER, 'HTTP_X_FORWARDED_FOR', FILTER_VALIDATE_IP );
-		} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-			return filter_var( $_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP );
+		if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+			return Pronamic_WP_Pay_Server::get( 'HTTP_X_FORWARDED_FOR', FILTER_VALIDATE_IP );
 		}
 
-		if ( filter_has_var( INPUT_SERVER, 'REMOTE_ADDR' ) ) {
-			return filter_input( INPUT_SERVER, 'REMOTE_ADDR', FILTER_VALIDATE_IP );
-		} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) )  {
-			return filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP );
+		if ( isset( $_SERVER['REMOTE_ADDR'] ) )  {
+			return Pronamic_WP_Pay_Server::get( 'REMOTE_ADDR', FILTER_VALIDATE_IP );
 		}
 
 		return '127.0.0.1';
