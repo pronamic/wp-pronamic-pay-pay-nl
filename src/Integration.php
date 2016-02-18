b3c89c28d510c59eeec70b190e2c51c9662789b8
Remco Tolsma <info@remcotolsma.nl>
2016-02-18 13:35:28 +0100

    Added get settings function.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Title: Pay.nl integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.1.3
+ * @since 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_PayNL_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'pay_nl';
@@ -23,5 +33,20 @@ class Pronamic_WP_Pay_Gateways_PayNL_Integration extends Pronamic_WP_Pay_Gateway
 
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_PayNL_Gateway';
+	}
+
+	/**
+	 * Get required settings for this integration.
+	 *
+	 * @see https://github.com/wp-premium/gravityforms/blob/1.9.16/includes/fields/class-gf-field-multiselect.php#L21-L42
+	 * @since 1.0.4
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = parent::get_settings();
+
+		$settings[] = 'pay_nl';
+
+		return $settings;
 	}
 }
