0c8282a8d0f46792c6a38cf84e9c6a3de6d96b16
Remco Tolsma <info@remcotolsma.nl>
2016-02-23 15:18:42 +0100

    Moved get_gateway_class() function to the configuration class.

--- src/Integration.php
+++ src/Integration.php
@@ -31,10 +31,6 @@ class Pronamic_WP_Pay_Gateways_PayNL_Integration extends Pronamic_WP_Pay_Gateway
 		return 'Pronamic_WP_Pay_Gateways_PayNL_Settings';
 	}
 
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_PayNL_Gateway';
-	}
-
 	/**
 	 * Get required settings for this integration.
 	 *
