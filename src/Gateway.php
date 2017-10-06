6362de79d65f3eae23486ddee5656ebc94a2ec04
Reüel van der Steege <reuel@pronamic.nl>
2017-10-06 16:14:56 +0200

    Set transaction sale data.

--- src/Gateway.php
+++ src/Gateway.php
@@ -119,6 +119,18 @@ class Pronamic_WP_Pay_Gateways_PayNL_Gateway extends Pronamic_WP_Pay_Gateway {
 				break;
 		}
 
+		// Set transaction sale data
+		// @see https://admin.pay.nl/docpanel/api/Transaction/start/4
+		$request['saleData'] = array(
+			'orderData'    => array(
+				array(
+					'description' => $payment->get_description(),
+					'price'       => Pronamic_WP_Util::amount_to_cents( $payment->get_amount() ),
+					'quantity'    => 1,
+				)
+			),
+		);
+
 		$result = $this->client->transaction_start(
 			$payment->get_amount(),
 			Pronamic_WP_Pay_Gateways_PayNL_Util::get_ip_address(),
