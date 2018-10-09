bbce199c2155a3b7b28d8138ad02c08aabe91461
Reüel van der Steege <reuel@pronamic.nl>
2018-10-09 14:49:39 +0200

    Working on post pay payment methods support.

--- src/Gateway.php
+++ src/Gateway.php
@@ -25,9 +25,16 @@ class Gateway extends Core_Gateway {
 	const SLUG = 'pay_nl';
 
 	/**
+	 * Client.
+	 *
+	 * @var Client
+	 */
+	protected $client;
+
+	/**
 	 * Constructs and initializes an Pay.nl gateway
 	 *
-	 * @param Config $config
+	 * @param Config $config Config.
 	 */
 	public function __construct( Config $config ) {
 		parent::__construct( $config );
@@ -36,7 +43,7 @@ class Gateway extends Core_Gateway {
 			'payment_status_request',
 		);
 
-		$this->set_method( Gateway::METHOD_HTTP_REDIRECT );
+		$this->set_method( self::METHOD_HTTP_REDIRECT );
 		$this->set_slug( self::SLUG );
 
 		$this->client = new Client( $config->token, $config->service_id );
@@ -70,58 +77,116 @@ class Gateway extends Core_Gateway {
 	 */
 	public function get_supported_payment_methods() {
 		return array(
+			PaymentMethods::AFTERPAY,
 			PaymentMethods::BANCONTACT,
 			PaymentMethods::BANK_TRANSFER,
 			PaymentMethods::CREDIT_CARD,
+			PaymentMethods::FOCUM,
 			PaymentMethods::GIROPAY,
 			PaymentMethods::IDEAL,
+			PaymentMethods::IN3,
+			PaymentMethods::KLARNA_PAY_LATER,
+			PaymentMethods::MAESTRO,
 			PaymentMethods::PAYPAL,
 			PaymentMethods::SOFORT,
 		);
 	}
 
 	/**
-	 * Start
+	 * Start.
 	 *
 	 * @see Core_Gateway::start()
 	 *
-	 * @param Payment $payment
+	 * @param Payment $payment Payment.
 	 */
 	public function start( Payment $payment ) {
 		$payment_method = $payment->get_method();
 
+		/*
+		 * New transaction request.
+		 * @link https://www.pay.nl/docs/developers.php#transactions
+		 */
+		$customer        = $payment->get_customer();
+		$birth_date      = $payment->get_customer()->get_birth_date();
+		$billing_address = $payment->get_billing_address();
+
+		// Payment lines.
+		$order_data = array();
+
+		foreach ( $payment->get_lines() as $line ) {
+			$order_data[] = array(
+				'productId'   => $line->get_id(),
+				'productType' => $line->get_type(), // ARTICLE, SHIPPING, HANDLING, DISCOUNT.
+				'description' => $line->get_description(),
+				'price'       => $line->get_unit_price(),
+				'quantity'    => $line->get_quantity(),
+			);
+		}
+
 		$request = array(
-			'enduser' => array(
-				'lastName'     => $payment->get_customer_name(),
-				'emailAddress' => $payment->get_email(),
+			// Transaction.
+			'transaction'     => array(
+				'currency'    => $payment->get_currency(),
+				'description' => $payment->get_description(),
+			),
+
+			// Payment method.
+			'paymentOptionId' => Methods::transform( $payment_method ),
+
+			// End user.
+			'enduser'         => array(
+				'initials'       => $customer->get_name()->get_first_name(),
+				'lastName'       => $customer->get_name()->get_last_name(),
+				'gender'         => $customer->get_gender(),
+				'dob'            => ( $birth_date instanceof \DateTime ) ? $birth_date->format( 'ddmmYYYY' ) : null,
+				'phoneNumber'    => $customer->get_phone(),
+				'emailAddress'   => $customer->get_email(),
+				'language'       => $customer->get_language(),
+
+				// Address.
+				'address'        => array(
+					'streetName'            => $billing_address->get_street_name(),
+					'streetNumber'          => $billing_address->get_house_number(),
+					'streetNumberExtension' => $billing_address->get_house_number_addition(),
+					'zipCode'               => $billing_address->get_postal_code(),
+					'city'                  => $billing_address->get_city(),
+					'countryCode'           => $billing_address->get_country_code(),
+				),
+
+				// Invoice address.
+				'invoiceAddress' => array(
+					'initials'              => $customer->get_name()->get_first_name(),
+					'lastName'              => $customer->get_name()->get_last_name(),
+					'gender'                => $customer->get_gender(),
+					'streetName'            => $billing_address->get_street_name(),
+					'streetNumber'          => $billing_address->get_house_number(),
+					'streetNumberExtension' => $billing_address->get_house_number_addition(),
+					'zipCode'               => $billing_address->get_postal_code(),
+					'city'                  => $billing_address->get_city(),
+					'countryCode'           => $billing_address->get_country_code(),
+				),
+			),
+
+			// Sale data.
+			'saleData'        => array(
+				'invoiceDate'  => $payment->get_date()->format( 'd-m-Y' ),
+				'deliveryDate' => $payment->get_date()->format( 'd-m-Y' ),
+				'orderData'    => $order_data,
 			),
 		);
 
-		switch ( $payment_method ) {
-			case PaymentMethods::IDEAL:
-				$request['paymentOptionId']    = Methods::IDEAL;
-				$request['paymentOptionSubId'] = $payment->get_issuer();
-
-				break;
-			default:
-				$method = Methods::transform( $payment_method );
-
-				if ( $method ) {
-					$request['paymentOptionId'] = $method;
-				}
-
-				if ( ! isset( $request['paymentOptionId'] ) && ! empty( $payment_method ) ) {
-					// Leap of faith if the WordPress payment method could not transform to a Mollie method?
-					$request['paymentOptionId'] = $payment_method;
-				}
+		// Check payment method.
+		if ( null === $request['paymentOptionId'] && ! empty( $payment_method ) ) {
+			// Leap of faith if the WordPress payment method could not transform to a Pay.nl method?
+			$request['paymentOptionId'] = $payment_method;
 		}
 
-		// Set transaction description.
-		// @see https://admin.pay.nl/docpanel/api/Transaction/start/4
-		$request['transaction'] = array(
-			'description' => $payment->get_description(),
-		);
+		// Set payment method specific parameters.
+		if ( PaymentMethods::IDEAL === $payment_method ) {
+			$request['paymentOptionSubId'] = $payment->get_issuer();
+		}
 
+		// Start transaction.
 		$result = $this->client->transaction_start(
 			$payment->get_amount()->get_amount(),
 			Util::get_ip_address(),
@@ -129,29 +194,31 @@ class Gateway extends Core_Gateway {
 			$request
 		);
 
+		// Handle errors.
 		if ( ! $result ) {
 			$this->error = $this->client->get_error();
 
 			return;
 		}
 
+		// Update gateway results in payment.
 		$payment->set_transaction_id( $result->transaction->transactionId );
 		$payment->set_action_url( $result->transaction->paymentURL );
 	}
 
 	/**
-	 * Update status of the specified payment
+	 * Update status of the specified payment.
 	 *
-	 * @param Payment $payment
+	 * @param Payment $payment Payment.
 	 */
 	public function update_status( Payment $payment ) {
+		// Get transaction info.
 		$result = $this->client->transaction_info( $payment->get_transaction_id() );
 
-		if ( isset( $result, $result->paymentDetails ) ) {
-			$state = $result->paymentDetails->state;
+		if ( isset( $result->paymentDetails ) ) {
+			$status = Statuses::transform( $result->paymentDetails->state );
 
-			$status = Statuses::transform( $state );
-
+			// Update payment status.
 			$payment->set_status( $status );
 		}
 	}
