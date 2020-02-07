46956dc893cab4e5e6de01b3ddb4f00aa6aba82d
Remco Tolsma <info@remcotolsma.nl>
2020-02-07 16:11:48 +0100

    Integration parent::__construct();.

--- src/Integration.php
+++ src/Integration.php
@@ -16,6 +16,8 @@ use Pronamic\WordPress\Pay\Gateways\Common\AbstractIntegration;
  */
 class Integration extends AbstractIntegration {
 	public function __construct() {
+		parent::__construct();
+
 		$this->id            = 'pay_nl';
 		$this->name          = 'Pay.nl';
 		$this->url           = 'https://www.pay.nl/';
