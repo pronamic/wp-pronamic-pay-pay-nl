b7b5722e801775e6b8c24f5ce13c3afdd7f0e747
Reüel van der Steege <reuel@pronamic.nl>
2021-01-18 16:05:45 +0100

    Happy 2021.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,7 @@ use Pronamic\WordPress\Pay\AbstractGatewayIntegration;
 /**
  * Title: Pay.nl integration
  * Description:
- * Copyright: 2005-2020 Pronamic
+ * Copyright: 2005-2021 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
--- src/Methods.php
+++ src/Methods.php
@@ -7,7 +7,7 @@ use Pronamic\WordPress\Pay\Core\PaymentMethods;
 /**
  * Title: Pay.nl payment methods
  * Description:
- * Copyright: 2005-2020 Pronamic
+ * Copyright: 2005-2021 Pronamic
  * Company: Pronamic
  *
  * @author  Reüel van der Steege
